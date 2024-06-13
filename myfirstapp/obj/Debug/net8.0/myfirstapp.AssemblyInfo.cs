ae02246c60351a47cd619e72bac217c9ad4c16b0
aratishejwal <shivdasshejwal311@gmail.com>
2024-06-13 10:10:21 +0530

    commit

--- myfirstapp/obj/Debug/net8.0/myfirstapp.AssemblyInfo.cs
+++ myfirstapp/obj/Debug/net8.0/myfirstapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4ab2f936fdf1d6783152e46f68e3e2968633a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
